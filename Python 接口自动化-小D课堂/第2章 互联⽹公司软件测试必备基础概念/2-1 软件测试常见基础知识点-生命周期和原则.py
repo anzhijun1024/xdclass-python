3ef17524bdd4649ebdf456c61a04b0d810ed11a2
anzhijun1024 <1010666711@qq.com>
2020-08-27 23:15:47 +0800

    python接口自动化学习

---
+++ Python 接口自动化-小D课堂/第2章 互联⽹公司软件测试必备基础概念/2-1 软件测试常见基础知识点-生命周期和原则.py
@@ -0,0 +1,34 @@
+
+
+
+
+
+def a():
+    a=2225566
+    return a
+
+
+
+
+def b(self):
+
+    # print(self)
+    return self
+
+
+
+
+def main():
+
+    # print(a())
+
+    c=a()
+
+    print(b(c))
+
+
+
+
+
+if __name__ == '__main__':
+    main()
